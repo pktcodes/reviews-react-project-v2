35f5fbee9ca708bffe455f5b660d5b65623247a7
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-08 12:35:18 -0500

    Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import people from "./data";
+
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = people[index];
+
+  console.log(name);
   return <h2>Reviews Starter</h2>;
 };
 export default App;
