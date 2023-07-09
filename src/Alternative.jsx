5a4cec582e793037b0e58934059952c6743cc870
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-08 23:57:56 -0500

    Modulus Operator - Alternative

---
+++ src/Alternative.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import people from "./data";
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  //   console.log(1 % 4);
+  //   console.log(2 % 4);
+  //   console.log(3 % 4);
+  //   console.log(4 % 4);
+  //   console.log(10 % 5);
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = (currentIndex - 1 + people.length) % people.length;
+      return newIndex;
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = (currentIndex + 1) % people.length;
+      return newIndex;
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = randomNumber + 1;
+    }
+    const newIndex = randomNumber % people.length;
+    setIndex(newIndex);
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button type="button" className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button type="button" className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button
+          type="button"
+          className="btn btn-hipster"
+          onClick={randomPerson}
+        >
+          surprise me
+        </button>
+      </article>
+    </main>
+  );
+};
+export default App;
