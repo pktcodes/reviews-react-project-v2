4b5e0e929de82a93e7f1685371d772deebed60cc
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-08 22:46:51 -0500

    Prev and Next

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
 
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return newIndex;
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex;
+    });
+  };
+
   return (
     <main>
       <article className="review">
@@ -18,6 +32,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button type="button" className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button type="button" className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
