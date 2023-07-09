7165dd1300e5e7bc81519fa27e067e4abb4e7799
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-08 21:15:54 -0500

    First Person

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import people from "./data";
-import { FaBeer } from "react-icons/fa";
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
 
-  console.log(name);
-
   return (
-    <div>
-      <h2>Reviews Starter</h2>
-      <FaBeer className="beer"></FaBeer>
-    </div>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
   );
 };
 export default App;
