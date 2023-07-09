019856f495bf001de1cc3753c4b409be722190f9
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-07-08 20:11:33 -0500

    React Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import people from "./data";
+import { FaBeer } from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
 
   console.log(name);
-  return <h2>Reviews Starter</h2>;
+
+  return (
+    <div>
+      <h2>Reviews Starter</h2>
+      <FaBeer className="beer"></FaBeer>
+    </div>
+  );
 };
 export default App;
